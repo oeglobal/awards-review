9392ddce5acafa15d37946e8a50f7478ee917cf2
Jure Cuhalev <jure@cuhalev.com>
2020-07-21 15:24:16 +0200

    Revert

--- awards/awardsapp/settings.py
+++ awards/awardsapp/settings.py
@@ -106,7 +106,7 @@ USE_TZ = False
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "../static"),
+    os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "../frontend/dist"),
 ]
 
