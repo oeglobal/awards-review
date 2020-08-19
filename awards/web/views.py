8bee4e8295609197b58be630443031b96f86a69e
Jure Cuhalev <jure@cuhalev.com>
2020-08-19 11:38:12 +0200

    Improve home page overview

--- awards/web/views.py
+++ awards/web/views.py
@@ -88,6 +88,18 @@ class SubmissionsView(LoginRequiredMixin, TemplateView):
 class StaffIndexView(StaffuserRequiredMixin, TemplateView):
     template_name = "web-staff/index.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update(
+            {
+                "drafts": Rating.drafts.count(),
+                "dones": Rating.dones.count(),
+                "conflicts": Rating.conflicts.count(),
+            }
+        )
+
+        return context
+
 
 class StaffSubmissionsView(StaffuserRequiredMixin, ListView):
     template_name = "web-staff/submissions.html"
